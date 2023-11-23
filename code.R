b6196eae2f705fbc43305ca5de28341754f6f8c3
Pascal Brissette <pascal.brissette@mcgill.ca>
2023-11-23 10:13:01 -0500

    Ajout fonction pour l'exploration des cooccurrences

--- code.R
+++ code.R
@@ -11,14 +11,19 @@ inst_ext_fun <- function(extension) {
 extensions <- c("LDAvis",
                 "topicmodels",
                 "data.table",
-                "text2vec")
+                "text2vec", 
+                "stringr",
+                "udpipe",
+                "dplyr",
+                "viridis",
+                "ggplot2")
 
 sapply(extensions, inst_ext_fun)
 
+# Nettoyage de l'environnement
 rm(list = ls())
 
-# Construction de deux modèles
-
+# Importation des structures de données
 lda.model.14.fr <- readRDS("data/output/lda_model_14_fr.RDS")
 lda.model.22.fr <- readRDS("data/output/lda_model_22_fr.RDS")
 lda.model.47.fr <- readRDS("data/output/lda_model_47_fr.RDS")
@@ -28,19 +33,21 @@ doc.topic.distr.14<- readRDS("data/output/doc_topic_distrib_14.RDS")
 doc.topic.distr.22 <- readRDS("data/output/doc_topic_distrib_22.RDS")
 doc.topic.distr.47 <- readRDS("data/output/doc_topic_distrib_47.RDS")
 
+# Autres données (à noter que les textes bruts ont été retirés de la structure `df.fr` pour respecter la license d'utilisation d'Eureka)
 df.fr <- readRDS("data/output/df_fr_lemma.RDS")
 dgTMatrix.fr <- readRDS("data/output/dgtMatrix_fr.RDS")
 dtm.tm.fr <- readRDS("data/output/dtm_tm_fr.RDS")
+annotation.fr <- readRDS("data/output/POS_fr_net.RDS")
 
 
 
-# Avec un modèle à 59 topics, le premier document est fortement associé aux thèmes 1, 17 et 53
+# Visualiser, pour le premier document seulement, les proportions des thèmes
 barplot(doc.topic.distr.14[1, ], xlab = "topic",
         ylab = "proportion", ylim = c(0, 0.4),
         names.arg = 1:ncol(doc.topic.distr.14))
 
 # Obtenir les mots les plus importants (chance qu'on les trouve dans un topic particulier, lambda = 1)
-lda.model.14.fr$get_top_words(n = 6, topic_number = c(8L, 2L, 13L, 11L), lambda = 0.5)
+lda.model.14.fr$get_top_words(n = 6, topic_number = c(14L, 1L, 7L, 4L), lambda = 0.5)
 
 
 # On peut retourner directement au texte, pour valider que les mots clés des trois thèmes résonnent avec le texte
@@ -69,5 +76,121 @@ doc.mots.rep.dt <- data.table(topic=1:dim(doc.topic.distr.14)[2],
                               most_rep_doc = most.rep.docs$lemma,
                               topic_top_words = top.words.list)
 
-# fwrite(doc.mots.rep.dt, "data/output/tableau_synthese_docs_topics.csv")
-# doc.mots.rep.dt <- fread("data/output/tableau_synthese_docs_topics.csv")
+
+#### Visualisation des thèmes des trois modèles avec LDAVis ----
+
+# Modèle avec 14 thèmes
+lda.model.14.fr$plot()
+
+# Modèle avec 14 thèmes
+lda.model.22.fr$plot()
+
+# Modèle avec 14 thèmes
+lda.model.47.fr$plot()
+
+
+
+#### Variance captée par les composantes dites principales (avancé) ----
+# L'outil LDAvis distribue les thèmes sur un plan en 2 dimensions.
+# Ces dimensions sont traversées par deux axes, PC1 et PC2. 
+# PC1 et PC2 sont les deux composantes principales, celles qui captent le maximum de variance dans le jeu de données.
+# On peut visualiser la proportion de cette variance captée par ces deux dimensions et les suivantes.
+
+# Modèle à 14 thèmes
+
+# Application de la fonction prcomp() à la matrice thèmes/mots
+pca_result = stats::prcomp(t(doc.topic.distr.14), scale. = TRUE)
+
+# Calcul de la valeur dite "eigenvalue" à partir de l'écart-type (sd)
+eigenvalues = pca_result$sdev^2
+
+# Calcul de la captation de la variance par les composantes
+composantes_variance <- sapply(eigenvalues, function(x) round((x/(sum(eigenvalues))*100)))
+
+# Visualisation de base (les valeurs en y sont des pourcentages)
+plot(composantes_variance)
+
+# Interprétation des résultats
+# Les deux axes principaux captent environ 35% de la variance
+
+
+
+
+
+#### Distribution chronologique des collocations ----
+# Première étape: Trouver les collocations
+# Step 1: Séparer les documents par groupe de 2 années
+setDT(df.fr)
+df.fr.lemma <- df.fr[, annee:=as.integer(str_sub(`Publication Date`, 1, 4))][
+  , .(doc_id, annee)
+]
+
+setDT(annotation.fr)
+
+# Vérification de l'identité des identifiants
+identical(annotation.fr[, .(doc_id), by="doc_id"][, .(doc_id)]$doc_id, df.fr.lemma$doc_id)
+
+# Ajout des années à la table udpipe
+annotation.fr <- data.table::merge.data.table(annotation.fr, df.fr.lemma, by="doc_id")
+
+
+
+data_by_year <- split(annotation.fr, annotation.fr$annee)
+
+# Étape 3: Calculer la cooccurrence pour chaque année
+cooccurrence_by_year <- lapply(data_by_year, function(x) {
+  temp <- cooccurrence(x = x$lemma, 
+                       relevant = x$upos %in% c("NOUN", "ADJ"),
+                       skipgram = 2)
+  temp$annee <- unique(x$annee)
+  return(temp)
+})
+
+# Assembler tous les groupes en une table unique
+all_cooc <- rbindlist(cooccurrence_by_year)
+
+all_cooc[,`:=`(cooc_terms = paste(term1, term2),
+               term1=NULL,
+               term2=NULL,
+               N=cooc,
+               cooc=NULL)]
+
+# Élimination de "arrondissement sud ouest"
+all_cooc <- all_cooc[!cooc_terms %in% c("arrondissement sud ouest",
+                                        "pied carré",
+                                        "comité exécutif",
+                                        "client création",
+                                        "client direction",
+                                        "direction création", 
+                                        "octobre heure",
+                                        "heure heure",
+                                        "sud ouest année")]
+
+
+top_cooc_per_year <- all_cooc[order(annee, -N)]
+
+top_n <- 3
+top_cooc_per_year <- top_cooc_per_year[, head(.SD, top_n), by = "annee"]
+
+#### Visualisation des cooccurrences en fonction d'années données ----
+# Example years to focus on
+selected_years <- c(2004, 2006, 2011, 2022)
+
+# Filter the data for the selected years and top 3 collocations per year
+filtered_data <- top_cooc_per_year %>%
+  filter(annee %in% selected_years) %>%
+  arrange(annee, -N) %>%
+  group_by(annee) %>%
+  slice_head(n = 3)
+
+# Plotting
+colors <- viridis(12)
+ggplot(filtered_data, aes(x = as.factor(annee), y = N, fill = cooc_terms)) +
+  geom_bar(stat = "identity", position = position_stack()) +
+  labs(x = "Année", y = "Fréquence", fill = "Collocation") +
+  theme_minimal() +
+  scale_fill_viridis_d(begin = 0.4)+
+  geom_text(aes(label = cooc_terms), position = position_stack(vjust = 0.5), size = 4) +
+  theme(text = element_text(size = 13))
+
+
